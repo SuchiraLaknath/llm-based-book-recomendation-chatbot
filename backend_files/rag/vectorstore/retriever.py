57b7523969d450a5ec72c0ba15f354fb23784342
Suchira Laknath Wanasinghe <suchiralaknath@gmail.com>
2024-07-16 22:54:52 +0530

    [wip] fix sementic issue, need to fix metadata store

--- backend_files/rag/vectorstore/retriever.py
+++ backend_files/rag/vectorstore/retriever.py
@@ -2,20 +2,20 @@ from .vectordb_ops import VectorDbOps
 from backend_files.read_config import Configurations
 
 class Retriver:
-    def __init__(self, configs) -> None:
+    def __init__(self, configs = None) -> None:
         if configs == None:
             configs = Configurations().get_config()
         vectordb_obj = VectorDbOps(create_vector_store =False)
-        self.vector_db - vectordb_obj.get_vectorstore()
+        self.vector_db = vectordb_obj.get_vectorstore()
         retriever_configs = configs['rag']['vectorstore']['retriever']
         self.k = retriever_configs['retriver_k']
         self.semantic_search_score_threshold = retriever_configs['semantic_search_score_threshold']
         self.search_type = retriever_configs['search_type']
 
     
-    def get_retriver(self, filtered_product_ids = [], max_results = None):
+    def get_retriever(self, filtered_product_ids = [], max_results = None):
         k = self.k
-        score_threshold = self.score_threshold
+        score_threshold = self.semantic_search_score_threshold
         search_type = "similarity_score_threshold"
         search_parameters = {"k": k}
         number_of_filtered_docs = len(filtered_product_ids)
